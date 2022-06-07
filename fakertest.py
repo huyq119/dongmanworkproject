a71950eddfab45caf935b3ed8a3d64b2a141a053
huyq119 <yuqi.hu@gmail.com>
2022-06-07 10:33:51 +0800

    更新faker代码

--- fakertest.py
+++ fakertest.py
@@ -8,17 +8,20 @@ ko_KR
 ja_JP
 '''
 from typing import List
-
 from faker import Faker
 import pyperclip
 import json
 
 fake = Faker(['zh_CN'])
 text_loc = []
+text_job = []
 for i in range(3):
     text_loc.append(str(fake.phone_number()))
-content = json.dumps(text_loc)
-print(content)
+    text_job.append(str(fake.job()))
+content_phone_number = json.dumps(text_loc, ensure_ascii=False)
+content_job = json.dumps(text_job, ensure_ascii=False)
+print(content_phone_number)
+print(content_job)
 
 
 
