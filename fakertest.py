b2a215d70b4ffdf1702f51ac6625e8d02ffb839a
huyq119 <yuqi.hu@gmail.com>
2022-06-07 14:14:44 +0800

    更新faker代码

--- fakertest.py
+++ fakertest.py
@@ -15,13 +15,22 @@ import json
 fake = Faker(['zh_CN'])
 text_loc = []
 text_job = []
+text_name = []
+text_paragraph = []
 for i in range(3):
     text_loc.append(str(fake.phone_number()))
     text_job.append(str(fake.job()))
+    text_name.append(str(fake.name()))
+    text_paragraph.append(fake.paragraph(nb_sentences=5, variable_nb_sentences=False))
+
 content_phone_number = json.dumps(text_loc, ensure_ascii=False)
 content_job = json.dumps(text_job, ensure_ascii=False)
+content_name = json.dumps(text_name, ensure_ascii=False)
+content_paragraph = json.dumps(text_paragraph, ensure_ascii=False)
 print(content_phone_number)
 print(content_job)
+print(content_name)
+print(content_paragraph)
 
 
 
