f6fed8953bd5544db5e92621b6ab0b6875e3e551
huyq119 <yuqi.hu@gmail.com>
2022-08-02 16:18:01 +0800

    添加pandas练习脚本

--- learningpandas.py
+++ learningpandas.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-url = "./tips.csv"
-tips = pd.read_csv(url)
-print(tips)
+# url = "./requests_1658998517.3500118.csv"
+# tips = pd.read_csv(url)
+rps = pd.read_excel("./titanic.xlsx")
+# tips.to_excel("titanic.xlsx", sheet_name="url", index=False)
+
+print(rps)
