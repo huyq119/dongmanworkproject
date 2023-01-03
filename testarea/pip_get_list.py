1bece1ea521c64f914004fc8bd42f56f01195dee
huyq119 <yuqi.hu@gmail.com>
2023-01-03 11:12:22 +0800

    更新

--- testarea/pip_get_list.py
+++ testarea/pip_get_list.py
@@ -4,7 +4,7 @@ with open("./worktest.txt") as f:
         i = num.split()
         a = i[0]
         b = i[2]
-        if a in ['tidevice', 'charset-normalizer']:
+        if a in ['tidevice', 'charset-normalizer', 'websocket-client', 'cryptography']:
             continue
         else:
             print(f"安装更新包{a}=={b}")
