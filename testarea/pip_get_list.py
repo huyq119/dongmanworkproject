b2eb7235a4a40dc0cc41725f24a7e5f231bd36d7
huyq119 <yuqi.hu@gmail.com>
2023-03-02 15:06:33 +0800

    更新

--- testarea/pip_get_list.py
+++ testarea/pip_get_list.py
@@ -1,11 +1,26 @@
 import os
+import subprocess
+
+
+# 定义安装更新包的函数
+def install_package(package_name):
+    print(f"安装更新包{package_name}")
+    cmd = ["pip3", "install", "--upgrade", package_name]
+    subprocess.run(cmd, check=True)
+
+
+# 指定要跳过更新的包名
+skip_list = ['tidevice', 'charset-normalizer', 'websocket-client', 'cryptography', 'greenlet', 'python-slugify']
+
 with open("./worktest.txt") as f:
     for num in f:
         i = num.split()
-        a = i[0]
-        b = i[2]
-        if a in ['tidevice', 'charset-normalizer', 'websocket-client', 'cryptography', 'greenlet']:
+        try:
+            a = i[0]
+            # b = i[2]
+        except IndexError:
+            print("列表为空或没有足够的元素")
             continue
-        else:
-            print(f"安装更新包{a}=={b}")
-            os.system(f"pip3 install --upgrade {a}")
+
+        if a not in skip_list:
+            install_package(a)
