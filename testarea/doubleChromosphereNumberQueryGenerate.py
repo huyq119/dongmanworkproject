89868a1745d5ea91ed979e6878b3fd2b13872433
huyq119 <yuqi.hu@gmail.com>
2023-03-29 12:39:33 +0800

    更新

--- testarea/doubleChromosphereNumberQueryGenerate.py
+++ testarea/doubleChromosphereNumberQueryGenerate.py
@@ -68,7 +68,7 @@ def creatLuckyNum():
     numbers_list = []
     for i in range(5):
         # blueball = random.randint(1, 16)
-        blueball = 4
+        blueball = 7
         luckynum1 = ''
         judgement = []
         r = 0
