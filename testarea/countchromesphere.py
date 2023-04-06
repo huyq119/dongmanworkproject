6b0815428c8ba53c4a46a490c7296b14d8dff9c8
huyq119 <yuqi.hu@gmail.com>
2023-04-06 11:30:05 +0800

    更新

--- testarea/countchromesphere.py
+++ testarea/countchromesphere.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 # 读取数据，指定空格为分隔符，没有列名
 df = pd.read_csv('./result_log.txt', sep=' ', header=None)
-
 value_counts = df.iloc[:, 9].value_counts()
-
 print(value_counts)
+
+# 统计DataFrame中的行数，即数据总数
+total_count = len(df)
+print("Total count:", total_count)
